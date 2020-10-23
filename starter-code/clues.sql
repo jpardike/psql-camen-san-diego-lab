31c0795d9063e2aa1223bfcd75d93132a9d8076e
jpardike2 <jpardike@gmail.com>
2020-10-23 16:33:30 -0700

    Deliverable finished. Found her in Santa Monica

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -24,32 +24,60 @@
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+-- SELECT a.name, a.code, a.region
+-- FROM country a, countrylanguage b
+-- WHERE language = 'Italian'
+-- AND b.countrycode = a.code
+-- AND a.region = 'Southern Europe'
+-- AND 1 = (SELECT COUNT(language)
+--          FROM countrylanguage
+--          WHERE countrycode = a.code);
 
+-- San Marino (SMR)
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+-- SELECT *
+-- FROM city
+-- WHERE countrycode = 'SMR'
+-- AND name <> 'San Marino';
 
+-- Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+-- SELECT *
+-- FROM city
+-- WHERE name LIKE 'Serr%';
 
+-- Serra (BRA)
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+-- SELECT a.name
+-- FROM city a, country b
+-- WHERE b.code = 'BRA'
+-- AND b.capital = a.id;
 
+-- Bras�lia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
 
+-- SELECT a.name
+-- FROM city a, country b
+-- WHERE b.code = 'BRA'
+-- AND b.capital = a.id;
 
+-- Bras�lia ( I got this answer in Clue #6 )
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
@@ -64,6 +92,8 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- SELECT name
+-- FROM city
+-- WHERE population = 91084;
 
-
--- She's in ______________________!
+-- She's in Santa Monica!
--- starter-code/query.sql
+++ starter-code/query.sql
@@ -4,6 +4,34 @@
 -- ORDER BY population
 -- LIMIT 1;
 
-SELECT *
-FROM countryLanguage
-WHERE countrycode = 'VAT';
+-- SELECT *
+-- FROM countryLanguage
+-- WHERE countrycode = 'VAT';
+
+-- SELECT a.name, a.code, a.region
+-- FROM country a, countrylanguage b
+-- WHERE language = 'Italian'
+-- AND b.countrycode = a.code
+-- AND a.region = 'Southern Europe'
+-- AND 1 = (SELECT COUNT(language)
+--          FROM countrylanguage
+--          WHERE countrycode = a.code);
+
+-- SELECT *
+-- FROM city
+-- WHERE countrycode = 'SMR'
+-- AND name <> 'San Marino';
+
+-- SELECT *
+-- FROM city
+-- WHERE name LIKE 'Serr%';
+
+-- SELECT a.name
+-- FROM city a, country b
+-- WHERE b.code = 'BRA'
+-- AND b.capital = a.id;
+
+
+-- SELECT name
+-- FROM city
+-- WHERE population = 91084;

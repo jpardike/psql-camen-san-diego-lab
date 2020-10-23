4328df6cdfadbedbe2b16f001821ac32faa20a0a
jpardike2 <jpardike@gmail.com>
2020-10-23 15:26:17 -0700

    Answered first two clues

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,13 +2,23 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+-- SELECT name, code, population
+-- FROM country
+-- WHERE region = 'Southern Europe'
+-- ORDER BY population
+-- LIMIT 1;
 
+-- Holy See (Vatican City State)
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+-- SELECT *
+-- FROM countryLanguage
+-- WHERE countrycode = 'VAT';
 
+-- Official Language is Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
---
+++ starter-code/query.sql
@@ -0,0 +1,9 @@
+-- SELECT name, code, population
+-- FROM country
+-- WHERE region = 'Southern Europe'
+-- ORDER BY population
+-- LIMIT 1;
+
+SELECT *
+FROM countryLanguage
+WHERE countrycode = 'VAT';
